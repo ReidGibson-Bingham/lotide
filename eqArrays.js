da79f4e45a67d6d5eacffe9666c1cd9bb255a6c5
P4ulH1ll <jsr3b0rn@gmail.com>
2022-02-09 03:37:14 +0000

    equal array function with assertion function

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`;
+  }
+
+};
+
+const eqArrays = function (list1, list2) {
+  let isArraySame = true;
+  let len = 0;
+  if (list1 > list2) {
+    len = list1.length;
+  } else {
+    len = list2.length;
+  }
+  for (let i = 0; i < len; i++) {
+    if (list1[i] !== list2[i]){
+      isArraySame = false;
+    }
+  }
+  return isArraySame;
+}
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
