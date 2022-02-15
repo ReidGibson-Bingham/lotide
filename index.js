d2df303af7d820b1e8daa43a2e7782d2b83e0d8d
P4ulH1ll <jsr3b0rn@gmail.com>
2022-02-15 04:33:58 +0000

    index.js file

---
+++ index.js
@@ -0,0 +1,21 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const countletters = require('./countletters');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle, 
+  eqArrays: eqArrays,
+  countletters: countletters,
+  findKey: findKey,
+  
+};
