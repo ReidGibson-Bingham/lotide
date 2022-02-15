6aa8f390db36ebb940a1ced0bbb57432eddb3b8f
P4ulH1ll <jsr3b0rn@gmail.com>
2022-02-15 02:48:14 +0000

    converting our test files into mocha and chai test files

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,4 @@
-
+/*
 const middle = require('../middle');
 
 console.log(middle([1])); // => []
@@ -9,3 +9,21 @@ console.log(middle([1, 2, 3, 4, 5]));
 
 console.log(middle([1, 2, 3, 4]));
 console.log(middle([1, 2, 3, 4, 5, 6]));
+
+*/
+
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.equal(middle([]), [1]);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.equal(middle([2]), [1, 2, 3]); 
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.equal(middle([1, 2, 3, 4]), [2, 3]); 
+  });
+
+});
